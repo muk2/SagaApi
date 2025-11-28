cbe8b4bca2ebaaf4f6b9822a2576b1c4f23c3a63
muk <themu321@gmail.com>
2025-11-27 20:07:42 -0500

    cors

--- src/main.py
+++ src/main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from controllers import auth_controller
 
 app = FastAPI()
 
+origins = [
+    "https://sagafe.vercel.app",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth_controller.router)
 
